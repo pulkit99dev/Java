7251bfb90dc7c593cac25335beaf8d9fcac4ff43
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-26 15:37:02 +0530

    prime num 2

--- src/L1_Basics/PrimeNum2.java
+++ src/L1_Basics/PrimeNum2.java
@@ -1,9 +1,30 @@
 package L1_Basics;
 
+import java.util.Scanner;
+
 public class PrimeNum2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int fact = 0;
+		int count = 2;
+		
+		while(count <= n-1) {
+			if(n % count == 0) {
+				fact = fact + 1;
+				break;
+			}
+			count = count + 1;
+			
+		}
+		if(fact > 0) {
+			System.out.println("Not Prime");
+		}else {
+			System.out.println("Prime");
+		}
 
 	}
 
