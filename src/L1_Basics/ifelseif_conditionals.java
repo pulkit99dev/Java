15d5a779c489b23db181cb7c22d459869eb9a83b
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-26 15:59:36 +0530

    if - else

--- src/L1_Basics/ifelseif_conditionals.java
+++ src/L1_Basics/ifelseif_conditionals.java
@@ -1,9 +1,26 @@
 package L1_Basics;
 
+import java.util.Scanner;
+
 public class ifelseif_conditionals {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		if(n > 90 && n <= 100) {
+			System.out.println("A grade");
+		}
+		if(n > 80 && n <= 90) {
+			System.out.println("B grade");
+		}
+		if(n > 70 && n <= 80) {
+			System.out.println("C grade");
+		}
+		if(n > 60 && n <= 70) {
+			System.out.println("D grade");
+		}
 
 	}
 
